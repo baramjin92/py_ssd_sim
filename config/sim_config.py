753b87c2a5433e2cb6e1315f86cc89bc296f9b0f
Yong Jin <baramjin92@gmail.com>
2020-08-28 10:22:51 +0900

    change nand configuration method

--- config/sim_config.py
+++ config/sim_config.py
@@ -3,6 +3,12 @@
 import os
 import sys
 
+import pandas as pd
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from config.ssd_param import *
+
 # define unit
 class unit_context :
 	def __init__(self) :
@@ -13,183 +19,309 @@ class unit_context :
 		self.scale_MiB = 1024*1024
 		self.scale_GiB = 1024*1024*1024
 
-# MLC NAND 128Gb
-class nand_256gb_mlc :
-	def __init__(self) :
-		# nand basic information
-		self.bits_per_cell = 2
-		self.size = 128 								# Gb
-		self.page_size = 16384					# byte
-		self.spare_size = 1024					# byte
-		self.page_num = 256
-		self.plane_num = 2	
-		self.main_block_num = 1024
-		self.add_block_num = 0
-		self.ext_block_num = 0
-		self.spare_block_num = self.add_block_num + self.ext_block_num
-		
-		self.MBB = 0.02
-		self.GBB = 0.005
-		self.BBR = self.MBB + self.GBB
-		
-		# nand ac paramter (size unit is byte, time unit is ns)	
-		# size
-		self.extra_data_size = 4
-		self.crc_size = 4*2
-		self.ecc_size = 192*2
-		
-		# time (ns)
-		self.nand_t_cna_w = 500	# tADL 300ns tcDQSH 100ns tWB 100ns 
-		self.nand_t_cna_r = 500
-		self.nand_t_cna_e = 500
-		self.nand_t_chk = 300		# tCS 20ns tWHR 120ns tRPRE 15ns
-		# NAND IF (400Mhz)
-		self.nand_t_xfer = 10000			# change value by calculation (2.5ns/byte) # (BYTES_PER_CHUNK + EXT_DATA_SIZE + ECC_SIZE + CRC_SIZE) * 5/2 + 500
-		
-		self.nand_t_read_lsb = 0
-		self.nand_t_read_msb = 0		
-		self.nand_t_read_full = (70*1000)
-		self.nand_t_read_half = (48*1000)
-		self.nand_t_prog_lsb = (498*1000)
-		self.nand_t_prog_msb = (1691*1000)				
-		self.nand_t_prog = (self.nand_t_prog_lsb + self.nand_t_prog_msb)
-		self.nand_t_prog_avg = int(self.nand_t_prog / 2)
-		self.nand_t_prog_slc = (398*1000)
-		self.nand_t_bers = (6500*1000)
-
-# TLC NAND 256Gb
-class nand_256gb_g3 :
-	def __init__(self) :
-		# nand basic information
-		self.bits_per_cell = 3
-		self.size = 256 						# Gb
-		self.page_size = 8*1024		# byte
-		self.spare_size = 1024
-		self.page_num = 1152
-		self.plane_num = 4		
-		self.main_block_num = 1214
-		self.add_block_num = 48
-		self.ext_block_num = 10
-		self.spare_block_num = self.add_block_num + self.ext_block_num
-		
-		self.MBB = 0.02
-		self.GBB = 0.005
-		self.BBR = self.MBB + self.GBB
-		
-		# nand ac paramter (size unit is byte, time unit is ns)	
-		# size
-		self.extra_data_size = 4
-		self.crc_size = 4*2
-		self.ecc_size = 192*2
-		
-		# time (ns)
-		self.nand_t_cna_w = 500	# tADL 300ns tcDQSH 100ns tWB 100ns 
-		self.nand_t_cna_r = 500
-		self.nand_t_cna_e = 500
-		self.nand_t_chk = 300		# tCS 20ns tWHR 120ns tRPRE 15ns
-		# NAND IF (533Mhz)
-		self.nand_t_xfer = 10000			# change value by calculation (2.5ns/byte) # (BYTES_PER_CHUNK + EXT_DATA_SIZE + ECC_SIZE + CRC_SIZE) * 5/2 + 500
-		
-		self.nand_t_read_lsb = 0
-		self.nand_t_read_msb = 0		
-		self.nand_t_read_full = (70*1000)
-		self.nand_t_read_half = (55*1000)
-		self.nand_t_prog_lsb = 0
-		self.nand_t_prog_msb = 0				
-		self.nand_t_prog = (2100*1000)			# one shot time
-		self.nand_t_prog_avg = int(self.nand_t_prog / 3)
-		self.nand_t_prog_slc = (300*1000)
-		self.nand_t_bers = (10000*1000)
-		
-class nand_256gb_g4 :
-	def __init__(self) :
-		# nand basic information
-		self.bits_per_cell = 3		
-		self.size = 256 						# Gb
-		self.page_size = 8*1024		# byte
-		self.page_num = 1728
-		self.plane_num = 4		
-		self.main_block_num = 1214
-		self.add_block_num = 48
-		self.ext_block_num = 10
-		self.spare_block_num = self.add_block_num + self.ext_block_num
-		
-		self.MBB = 0.02
-		self.GBB = 0.005
-		self.BBR = self.MBB + self.GBB
-		
-		# nand ac paramter (size unit is byte, time unit is ns)	
-		# size
-		self.extra_data_size = 4
-		self.crc_size = 4*2
-		self.ecc_size = 192*2
-		
-		# time (ns)
-		self.nand_t_cna_w = 500	# tADL 300ns tcDQSH 100ns tWB 100ns 
-		self.nand_t_cna_r = 500
-		self.nand_t_cna_e = 500
-		self.nand_t_chk = 300		# tCS 20ns tWHR 120ns tRPRE 15ns
-		# NAND IF (667Mhz)
-		self.nand_t_xfer = 10000			# change value by calculation (2.5ns/byte) # (BYTES_PER_CHUNK + EXT_DATA_SIZE + ECC_SIZE + CRC_SIZE) * 5/2 + 500
-		
-		self.nand_t_read_lsb = 0
-		self.nand_t_read_msb = 0
-		self.nand_t_read_full = (50*1000)
-		self.nand_t_read_half = (45*1000)
-		self.nand_t_prog_lsb = 0
-		self.nand_t_prog_msb = 0		
-		self.nand_t_prog = (1770*1000)			# one shot time	
-		self.nand_t_prog_avg = self.nand_t_prog / 3
-		self.nand_t_prog_slc = (320*1000)
-		self.nand_t_bers = (10000*1000)		
-
-class nand_512gb_g5 :
-	def __init__(self) :
-		# nand basic information
-		self.bits_per_cell = 3		
-		self.size = 512 						# Gb
-		self.page_size = 16*1024		# byte
-		self.page_num = 1152
-		self.plane_num = 4		
-		self.main_block_num = 910
-		self.add_block_num = 48
-		self.ext_block_num = 10
-		self.spare_block_num = self.add_block_num + self.ext_block_num
-		
-		self.MBB = 0.02
-		self.GBB = 0.005
-		self.BBR = self.MBB + self.GBB
-		
-		# nand ac paramter (size unit is byte, time unit is ns)	
-		# size
-		self.extra_data_size = 4
-		self.crc_size = 4*2
-		self.ecc_size = 192*2
-		
-		# time (ns)
-		self.nand_t_cna_w = 500	# tADL 300ns tcDQSH 100ns tWB 100ns 
-		self.nand_t_cna_r = 500
-		self.nand_t_cna_e = 500
-		self.nand_t_chk = 300		# tCS 20ns tWHR 120ns tRPRE 15ns
-		# NAND IF (800Mhz)
-		self.nand_t_xfer = 10000			# change value by calculation (2.5ns/byte) # (BYTES_PER_CHUNK + EXT_DATA_SIZE + ECC_SIZE + CRC_SIZE) * 5/2 + 500
-		
-		self.nand_t_read_lsb = 0
-		self.nand_t_read_msb = 0
-		self.nand_t_read_full = (61*1000)
-		self.nand_t_read_half = (45*1000)
-		self.nand_t_prog_lsb = 0
-		self.nand_t_prog_msb = 0		
-		self.nand_t_prog = (1650*1000)			# one shot time	
-		self.nand_t_prog_avg = self.nand_t_prog / 3
-		self.nand_t_prog_slc = (300*1000)
-		self.nand_t_bers = (10000*1000)		
-											
 unit = unit_context()
-#nand_info = nand_256gb_mlc()
-nand_info = nand_256gb_g3()
-#nand_info = nand_256gb_g4()
-#nand_info = nand_512gb_g5()
+
+nand_256gb_mlc = {
+		'bits_per_cell' : 2,
+		'size' : 128, 									# Gb
+		'page_size' : 16384,					# byte
+		'spare_size' : 1024,					# byte
+		'page_num' : 256,
+		'plane_num' : 2,	
+		'main_block_num' : 1024,
+		'add_block_num' : 0,
+		'ext_block_num' : 0,
+		'spare_block_num' : 0, 				# add_block_num + ext_block_num
+		
+		'MBB' : 0.02,
+		'GBB' : 0.005,
+		'BBR' : 0,										# MBB + GBB
+		
+		# nand ac paramter (size unit is byte, time unit is ns)			
+		# size (byte)
+		'extra_data_size' : 4,
+		'crc_size' : 4*2,
+		'ecc_size' : 192*2,
+		
+		# NAND IF (Mhz)				
+		'nand_if' : 400,
+				
+		# time (ns)
+		'nand_t_cna_w' : 500,				# tADL 300ns tcDQSH 100ns tWB 100ns 
+		'nand_t_cna_r' : 500,
+		'nand_t_cna_e' : 500,
+		'nand_t_chk' : 300,					# tCS 20ns tWHR 120ns tRPRE 15ns
+		
+		'nand_t_read_lsb' : 0,
+		'nand_t_read_msb' : 0,		
+		'nand_t_read_full' : 70*1000,
+		'nand_t_read_half' : 48*1000,
+		'nand_t_read_slc' : 30*1000,
+		'nand_t_prog_lsb' : 498*1000,
+		'nand_t_prog_msb' : 1691*1000,		
+		'nand_t_prog' : (498+169)*1000,
+		'nand_t_prog_avg' : (498+169)*1000/2,
+		'nand_t_prog_slc' : 398*1000,
+		'nand_t_bers' : 6500*1000
+}
+
+nand_256gb_g3 = {
+		'bits_per_cell' : 3,
+		'size' : 256, 									# Gb
+		'page_size' : 8192,					# byte
+		'spare_size' : 1024,					# byte
+		'page_num' : 1152,
+		'plane_num' : 4,	
+		'main_block_num' : 1214,
+		'add_block_num' : 48,
+		'ext_block_num' : 10,
+		'spare_block_num' : 0, 				# add_block_num + ext_block_num
+		
+		'MBB' : 0.02,
+		'GBB' : 0.005,
+		'BBR' : 0,										# MBB + GBB
+		
+		# nand ac paramter (size unit is byte, time unit is ns)			
+		# size (byte)
+		'extra_data_size' : 4,
+		'crc_size' : 4*2,
+		'ecc_size' : 192*2,
+
+		# NAND IF (Mhz)				
+		'nand_if' : 533,
+								
+		# time (ns)
+		'nand_t_cna_w' : 500,				# tADL 300ns tcDQSH 100ns tWB 100ns 
+		'nand_t_cna_r' : 500,
+		'nand_t_cna_e' : 500,
+		'nand_t_chk' : 300,					# tCS 20ns tWHR 120ns tRPRE 15ns
+		
+		'nand_t_read_lsb' : 0,
+		'nand_t_read_msb' : 0,		
+		'nand_t_read_full' : 70*1000,
+		'nand_t_read_half' : 55*1000,
+		'nand_t_read_slc' : 30*1000,
+		'nand_t_prog_lsb' : 0,
+		'nand_t_prog_msb' : 0,		
+		'nand_t_prog' : 2100*1000,
+		'nand_t_prog_avg' : 2100*1000/3,
+		'nand_t_prog_slc' : 300*1000,
+		'nand_t_bers' : 10000*1000
+}
+
+nand_256gb_g4 = {
+		'bits_per_cell' : 3,
+		'size' : 256, 									# Gb
+		'page_size' : 8192,					# byte
+		'spare_size' : 1024,					# byte
+		'page_num' : 1278,
+		'plane_num' : 4,	
+		'main_block_num' : 1214,
+		'add_block_num' : 48,
+		'ext_block_num' : 10,
+		'spare_block_num' : 0, 				# add_block_num + ext_block_num
+		
+		'MBB' : 0.02,
+		'GBB' : 0.005,
+		'BBR' : 0,										# MBB + GBB
+		
+		# nand ac paramter (size unit is byte, time unit is ns)			
+		# size (byte)
+		'extra_data_size' : 4,
+		'crc_size' : 4*2,
+		'ecc_size' : 192*2,
+
+		# NAND IF (Mhz)				
+		'nand_if' : 667,
+								
+		# time (ns)
+		'nand_t_cna_w' : 500,				# tADL 300ns tcDQSH 100ns tWB 100ns 
+		'nand_t_cna_r' : 500,
+		'nand_t_cna_e' : 500,
+		'nand_t_chk' : 300,					# tCS 20ns tWHR 120ns tRPRE 15ns
+		
+		'nand_t_read_lsb' : 0,
+		'nand_t_read_msb' : 0,		
+		'nand_t_read_full' : 50*1000,
+		'nand_t_read_half' : 45*1000,
+		'nand_t_read_slc' : 30*1000,
+		'nand_t_prog_lsb' : 0,
+		'nand_t_prog_msb' : 0,		
+		'nand_t_prog' : 1770*1000,
+		'nand_t_prog_avg' : 1770*1000/3,
+		'nand_t_prog_slc' : 320*1000,
+		'nand_t_bers' : 10000*1000
+}
+
+nand_512gb_g5 = {
+		'bits_per_cell' : 3,
+		'size' : 512, 									# Gb
+		'page_size' : 16*1024,					# byte
+		'spare_size' : 1024,					# byte
+		'page_num' : 1152,
+		'plane_num' : 4,	
+		'main_block_num' : 910,
+		'add_block_num' : 48,
+		'ext_block_num' : 10,
+		'spare_block_num' : 0, 				# add_block_num + ext_block_num
+		
+		'MBB' : 0.02,
+		'GBB' : 0.005,
+		'BBR' : 0,										# MBB + GBB
+		
+		# nand ac paramter (size unit is byte, time unit is ns)			
+		# size (byte)
+		'extra_data_size' : 4,
+		'crc_size' : 4*2,
+		'ecc_size' : 192*2,
+
+		# NAND IF (Mhz)				
+		'nand_if' : 800,
+				
+		# time (ns)
+		'nand_t_cna_w' : 500,				# tADL 300ns tcDQSH 100ns tWB 100ns 
+		'nand_t_cna_r' : 500,
+		'nand_t_cna_e' : 500,
+		'nand_t_chk' : 300,					# tCS 20ns tWHR 120ns tRPRE 15ns
+		
+		'nand_t_read_lsb' : 0,
+		'nand_t_read_msb' : 0,		
+		'nand_t_read_full' : 61*1000,
+		'nand_t_read_half' : 45*1000,
+		'nand_t_read_slc' : 30*1000,
+		'nand_t_prog_lsb' : 0,
+		'nand_t_prog_msb' : 0,		
+		'nand_t_prog' : 1650*1000,
+		'nand_t_prog_avg' : 1650*1000/3,
+		'nand_t_prog_slc' : 300*1000,
+		'nand_t_bers' : 10000*1000
+}
+
+class nand_config :
+	def __init__(self, nand_param) :
+		# nand basic information
+		self.bits_per_cell = nand_param['bits_per_cell']
+		self.size = nand_param['size'] 								# Gb
+		self.page_size = nand_param['page_size']					# byte
+		self.spare_size = nand_param['spare_size']					# byte
+		self.page_num = nand_param['page_num']
+		self.plane_num = nand_param['plane_num']
+		self.main_block_num = nand_param['main_block_num']
+		self.add_block_num = nand_param['add_block_num']
+		self.ext_block_num = nand_param['ext_block_num']
+		self.spare_block_num = self.add_block_num + self.ext_block_num
+		
+		self.MBB = nand_param['MBB']
+		self.GBB = nand_param['GBB']
+		self.BBR = self.MBB + self.GBB
+		
+		# nand ac paramter (size unit is byte, time unit is ns)			
+		# size
+		self.extra_data_size = nand_param['extra_data_size']
+		self.crc_size = nand_param['crc_size']
+		self.ecc_size = nand_param['ecc_size']
+
+		# NAND IF
+		self.nand_if = nand_param['nand_if']			
+		# change value by calculation when nand if is 400MHz (2.5ns/byte) # (5/2) *(BYTES_PER_CHUNK + EXT_DATA_SIZE + ECC_SIZE + CRC_SIZE)  + 500
+		self.nand_t_xfer = (1/800*1000) * (BYTES_PER_CHUNK + self.extra_data_size + self.ecc_size + self.crc_size) + 500
+				
+		# time (ns)
+		self.nand_t_cna_w = nand_param['nand_t_cna_w']	# tADL 300ns tcDQSH 100ns tWB 100ns 
+		self.nand_t_cna_r = nand_param['nand_t_cna_r']
+		self.nand_t_cna_e = nand_param['nand_t_cna_e']
+		self.nand_t_chk = nand_param['nand_t_chk']		# tCS 20ns tWHR 120ns tRPRE 15ns
+						
+		self.nand_t_read_lsb = nand_param['nand_t_read_lsb']
+		self.nand_t_read_msb = nand_param['nand_t_read_msb']
+		self.nand_t_read_full = nand_param['nand_t_read_full']
+		self.nand_t_read_half = nand_param['nand_t_read_half']
+		self.nand_t_read_slc = nand_param['nand_t_read_slc']
+		self.nand_t_prog_lsb = nand_param['nand_t_prog_lsb']
+		self.nand_t_prog_msb = nand_param['nand_t_prog_msb']
+		if self.bits_per_cell == 2 :
+			self.nand_t_prog = (self.nand_t_prog_lsb + self.nand_t_prog_msb)
+		else :
+			self.nand_t_prog = nand_param['nand_t_prog']
+		self.nand_t_prog_avg = int(self.nand_t_prog / self.bits_per_cell)
+		self.nand_t_prog_slc = nand_param['nand_t_prog_slc']
+		self.nand_t_bers = nand_param['nand_t_bers']
+										
+	def get_type_label(self) :
+		return {'name' : ['bits per cell', 'capacity[Gb]', 'page size[KB]', 'page num', 'plane num', 'wordline num', 'block num', 'small block size[MB]', 'big block size[MB]', 'extra_data_size', 'crc_size', 'ecc_size']}				
+	
+	def get_type_value(self) :
+		# calculate ssd information
+		small_block_size = self.page_size * self.page_num
+		big_block_size = small_block_size * self.plane_num
+													
+		info_columns = []
+		info_columns.append(self.bits_per_cell)
+		info_columns.append(self.size)
+		info_columns.append(int(self.page_size / unit.scale_KB))
+		info_columns.append(self.page_num)
+		info_columns.append(self.plane_num)
+		info_columns.append(int(self.page_num / self.bits_per_cell))
+		info_columns.append(self.main_block_num)
+		info_columns.append(int(small_block_size / unit.scale_MB))
+		info_columns.append(int(big_block_size / unit.scale_MB))
+		info_columns.append(self.extra_data_size)
+		info_columns.append(self.crc_size)
+		info_columns.append(self.ecc_size)
+
+		return info_columns
+
+	def get_param_lable(self) :
+		return {'name' : ['interface speed', 'nand_t_xfer(us/4KB)', 'nand_t_xfer(us/multi-plane)', 'nand_t_cna_w', 'nand_t_cna_r', 'nand_t_cna_e', 'nand_t_chk', 'nand_t_xfer', 'nand_t_read_full [us]', 'nand_t_read_half [us]', 'nand_t_read_slc [us]', 'nand_t_prog [us]', 'nand_t_prog_avg [us]', 'nand_t_prog_slc [us]', 'nand_t_bers [ms]']}
+		
+	def get_param_value(self) :
+		param_columns = []
+
+		param_columns.append(self.nand_if)
+		param_columns.append(int(self.nand_t_xfer/1000))
+		
+		num_chunks = self.page_size / BYTES_PER_CHUNK * self.plane_num		
+		param_columns.append(int(self.nand_t_xfer * num_chunks /1000))
+				
+		param_columns.append(self.nand_t_cna_w)
+		param_columns.append(self.nand_t_cna_r)
+		param_columns.append(self.nand_t_cna_e)
+		param_columns.append(self.nand_t_chk)
+		param_columns.append(self.nand_t_xfer)
+
+		param_columns.append(int(self.nand_t_read_full/1000))
+		param_columns.append(int(self.nand_t_read_half/1000))
+		param_columns.append(int(self.nand_t_read_slc/1000))
+				
+		param_columns.append(int(self.nand_t_prog/1000))
+		param_columns.append(int(self.nand_t_prog_avg/1000))
+		param_columns.append(int(self.nand_t_prog_slc/1000))
+		param_columns.append(int(self.nand_t_bers/1000000))
+
+		return param_columns
+																				
+	def print_type(self) :
+		print('\nnand type')
+							
+		info_pd = pd.DataFrame(self.get_type_label())																																	
+		info_pd['value'] = pd.Series(self.get_type_value(), index=info_pd.index)
+
+		print(info_pd)											
+
+	def print_param(self) :
+		print('\nnand parameter')
+														
+		param_pd = pd.DataFrame(self.get_param_lable())																																																																																												
+		param_pd['value'] = pd.Series(self.get_param_value(), index=param_pd.index)
+		
+		print(param_pd)								
+													
+nand_info = nand_config(nand_256gb_mlc)
 
 if __name__ == '__main__' :
-	print('nand configuration')												
+	print('nand configuration')				
+	
+	nand1 = nand_config(nand_256gb_g3)
+	nand1.print_type()
+	nand1.print_param()
+																		
